d227d2e01d06a5dcf106d1786ed89a787a922108
Rob van Putten <breinbaasnl@gmail.com>
2023-07-14 11:47:44 +0200

    added stix parser

--- dgeotoolkit/dstability/parser.py
+++ dgeotoolkit/dstability/parser.py
@@ -1,6 +1,9 @@
+from zipfile import ZipFile
+from pathlib import Path
+
 from ..dstability.dstabilitymodel import DStabilityModel
 from ..helpers import case_insensitive_glob
-from ..shared.json_conversions import object_from_file
+from ..shared.json_conversions import object_from_file, object_from_str
 
 
 class DGeoStabilityParser:
@@ -18,7 +21,22 @@ class DGeoStabilityParser:
         return dsm
 
     def parse(self, stix_path: str) -> DStabilityModel:
-        pass
+        dsm = DStabilityModel()
+        with ZipFile(stix_path) as zip:
+            for f in zip.filelist:
+                if f.filename.find(".json") == -1:
+                    continue  # skip checksum
+                cname = Path(f.filename).stem.capitalize()
+                s = zip.read(f).decode(errors="ignore")
+
+                try:
+                    instance = object_from_str(s, cname)
+                    iname = instance.__class__.__name__.replace("Object", "")
+                    setattr(dsm, iname, instance)
+                except Exception as e:
+                    raise ValueError(f"Could not handle {f}, got error '{e}'")
+
+        return dsm
 
     def serialize(self, path: str):
         pass
--- tests/dstability/test_parser.py
+++ tests/dstability/test_parser.py
@@ -1,7 +1,12 @@
 from dgeotoolkit.dstability.parser import DGeoStabilityParser
 
 
-def test_read_unpacked():
+def test_parse_unpacked():
     dgp = DGeoStabilityParser()
     dsm = dgp.parse_unpacked("tests/testdata/dstability/unpacked")
     i = 1
+
+
+def test_parse():
+    dgp = DGeoStabilityParser()
+    dsm = dgp.parse("tests/testdata/dstability/sample01.stix")
